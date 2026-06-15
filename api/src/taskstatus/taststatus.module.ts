be1986c581e7dfc2d50a1842c06de4068da2761a
Cameron Stephen <cameron.stephen12@gmail.com>
2026-06-16 00:47:48 +0100

    changed deploy-api to not delete production database

--- api/src/taskstatus/taststatus.module.ts
+++ api/src/taskstatus/taststatus.module.ts
@@ -3,7 +3,7 @@ import { TaskStatusService } from './taskstatus.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([])],
+  imports: [TypeOrmModule],
   providers: [TaskStatusService],
   exports: [TaskStatusService],
 })
